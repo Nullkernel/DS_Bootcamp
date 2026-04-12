a9ec2dd26c82644123bff906940ca6f1e4fdf9a0
Amal Bijoy <amalbijoy2007@gmail.com>
2026-04-13 00:11:41 +0530

    Update and rename Data-Structures/Linked-Lists/DoubleLinkedList.java to Data-Structures/Linked-Lists/DoubleLinkedList/DoubleLinkedList.java

--- Data-Structures/Linked-Lists/DoubleLinkedList/DoubleLinkedList.java
+++ Data-Structures/Linked-Lists/DoubleLinkedList/DoubleLinkedList.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
 
-class DoublyLinkedList {
+class DoublyLinkedLists {
     private static class Node {
         int data;
         Node prev;
         Node next;
-
         Node(int data) {
             this.data = data;
             this.prev = null;
             this.next = null;
         }
     }
-
     private Node head;
-
     public void insertAtBeginning(int data) {
         Node newNode = new Node(data);
-
         if (head == null) {
             head = newNode;
         } else {
@@ -25,74 +21,58 @@ class DoublyLinkedList {
             head.prev = newNode;
             head = newNode;
         }
-
         System.out.println(data + " Inserted at the Beginning.");
     }
-
     public void insertAtEnd(int data) {
         Node newNode = new Node(data);
-
         if (head == null) {
             head = newNode;
             System.out.println(data + " Inserted at the End.");
             return;
         }
-
         Node temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
-
         temp.next = newNode;
         newNode.prev = temp;
-
         System.out.println(data + " Inserted at the End.");
     }
-
     public void deleteFromBeginning() {
         if (head == null) {
             System.out.println("The List is Empty. Can't Delete element.");
             return;
         }
-
         System.out.println(head.data + " Deleted from Beginning.");
         head = head.next;
-
         if (head != null) {
             head.prev = null;
         }
     }
-
     public void deleteFromEnd() {
         if (head == null) {
             System.out.println("The List is Empty. Can't Delete element.");
             return;
         }
-
         if (head.next == null) {
             System.out.println(head.data + " Deleted from End.");
             head = null;
             return;
         }
-
         Node temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
-
         System.out.println(temp.data + " Deleted from End.");
         temp.prev.next = null;
     }
-
     public void search(int key) {
         if (head == null) {
             System.out.println("The List is Empty!");
             return;
         }
-
         Node temp = head;
         int pos = 1;
-
         while (temp != null) {
             if (temp.data == key) {
                 System.out.println(key + " Found at position: " + pos);
@@ -101,37 +81,30 @@ class DoublyLinkedList {
             temp = temp.next;
             pos++;
         }
-
         System.out.println(key + " NOT Found in the List.");
     }
-
     public void displayForward() {
         if (head == null) {
             System.out.println("The List is Empty!");
             return;
         }
-
         System.out.println("Forward Traversal:");
         Node temp = head;
-
         while (temp != null) {
             System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
         System.out.println("null");
     }
-
     public void displayBackward() {
         if (head == null) {
             System.out.println("The List is Empty!");
             return;
         }
-
         Node temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
-
         System.out.println("Backward Traversal:");
         while (temp != null) {
             System.out.print(temp.data + " -> ");
@@ -140,15 +113,13 @@ class DoublyLinkedList {
         System.out.println("null");
     }
 }
-
 public class DoubleLinkedList {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        DoublyLinkedList dll = new DoublyLinkedList();
+        DoublyLinkedLists dll = new DoublyLinkedLists();
         int choice, data;
-
         do {
-            System.out.println("\n--- Linked List Operations Menu ---");
+            System.out.println("\n--- Double Linked List Operations Menu ---");
             System.out.println("1. Insert at Beginning");
             System.out.println("2. Insert at End");
             System.out.println("3. Delete from Beginning");
@@ -158,53 +129,42 @@ public class DoubleLinkedList {
             System.out.println("7. Display Backward Direction");
             System.out.println("8. Exit");
             System.out.print("Enter your choice: ");
-
             choice = sc.nextInt();
-
             switch (choice) {
                 case 1:
                     System.out.print("Enter element to Insert at Beginning: ");
                     data = sc.nextInt();
                     dll.insertAtBeginning(data);
                     break;
-
                 case 2:
                     System.out.print("Enter element to Insert at End: ");
                     data = sc.nextInt();
                     dll.insertAtEnd(data);
                     break;
-
                 case 3:
                     dll.deleteFromBeginning();
                     break;
-
                 case 4:
                     dll.deleteFromEnd();
                     break;
-
                 case 5:
                     System.out.print("Enter element to Search: ");
                     data = sc.nextInt();
                     dll.search(data);
                     break;
-
                 case 6:
                     dll.displayForward();
                     break;
-
                 case 7:
                     dll.displayBackward();
                     break;
-
                 case 8:
                     System.out.println("Exiting...");
                     break;
-
                 default:
                     System.out.println("Invalid choice!");
             }
         } while (choice != 8);
-
         sc.close();
     }
 }
